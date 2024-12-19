09ad2191a280e346e913f8ef4aea5751e07a634e
Myo-Khant11Aung <myokhantaung2004@gmail.com>
2024-12-19 15:59:31 -0800

    Add task

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,125 +1,53 @@
-// import React, { Component } from "react";
-// import "./App.css";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTask } from "./taskActions";
 
-// class Main extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       tasks: [],
-//       value: "",
-//       editID: null,
-//       editValue: "",
-//     };
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskName: "",
+    };
+  }
 
-//     this.onChange = this.onChange.bind(this);
-//     this.onAddTask = this.onAddTask.bind(this);
-//     this.onDeleteTask = this.onDeleteTask.bind(this);
-//     this.onEditChange = this.onEditChange.bind(this);
-//     this.onEditTask = this.onEditTask.bind(this);
-//     this.onConfirm = this.onConfirm.bind(this);
-//     this.onCancel = this.onCancel.bind(this);
-//     this.renderEdit = this.renderEdit.bind(this);
-//   }
+  onChange = (event) => {
+    this.setState({ taskName: event.target.value });
+  };
 
-//   onChange = (event) => {
-//     this.setState({ value: event.target.value });
-//   };
+  onAddTask = (tempValue) => {
+    tempValue.preventDefault();
+    const task = {
+      id: Date.now(),
+      name: this.state.taskName,
+    };
+    this.props.addTask(task);
+    this.setState({ taskName: "" });
+  };
 
-//   onAddTask = (event) => {
-//     event.preventDefault();
-//     const taskobj = {
-//       taskname: this.state.value,
-//       id: Date.now(),
-//     };
-//     if (this.state.value.trim() !== "") {
-//       this.setState((prevState) => ({
-//         tasks: [...prevState.tasks, taskobj],
-//         value: "",
-//       }));
-//     }
-//   };
+  render() {
+    const taskInListForm = this.props.tasks.map((eachTask) => (
+      <li key={eachTask.id}>{eachTask.name}</li>
+    ));
+    return (
+      <form>
+        <h1>To Do App</h1>
+        <input
+          placeholder="Type your task"
+          onChange={this.onChange}
+          value={this.state.taskName}
+        ></input>
+        <button onClick={this.onAddTask}>Add Task</button>
+        <ul>{taskInListForm}</ul>
+      </form>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  tasks: state.tasks,
+});
 
-//   onDeleteTask = (itemId) => {
-//     this.setState((prevState) => ({
-//       tasks: prevState.tasks.filter((task) => task.id !== itemId),
-//     }));
-//   };
+const mapDispatchToProps = {
+  addTask,
+};
 
-//   onEditChange = (event) => {
-//     this.setState({ editValue: event.target.value });
-//   };
-
-//   onEditTask = (id, currentName) => {
-//     this.setState({
-//       editID: id,
-//       editValue: currentName,
-//     });
-//   };
-
-//   onConfirm = (id) => {
-//     this.setState((prevState) => ({
-//       tasks: prevState.tasks.map((task) =>
-//         task.id === id ? { ...task, taskname: prevState.editValue } : task
-//       ),
-//       editID: null,
-//       editValue: "",
-//     }));
-//   };
-
-//   onCancel = () => {
-//     this.setState({
-//       editID: null,
-//       editValue: "",
-//     });
-//   };
-
-//   renderEdit = (task) => (
-//     <form onSubmit={(event) => event.preventDefault()}>
-//       <input
-//         type="text"
-//         value={this.state.editValue}
-//         onChange={this.onEditChange}
-//       />
-//       <button type="button" onClick={() => this.onConfirm(task.id)}>
-//         Confirm
-//       </button>
-//       <button type="button" onClick={this.onCancel}>
-//         Cancel
-//       </button>
-//     </form>
-//   );
-
-//   render() {
-//     const tempList = this.state.tasks.map((task) => (
-//       <li key={task.id}>
-//         {this.state.editID === task.id ? (
-//           this.renderEdit(task)
-//         ) : (
-//           <div>
-//             {task.taskname}
-//             <button onClick={() => this.onDeleteTask(task.id)}>Delete</button>
-//             <button onClick={() => this.onEditTask(task.id, task.taskname)}>
-//               Edit
-//             </button>
-//           </div>
-//         )}
-//       </li>
-//     ));
-
-//     return (
-//       <div className="todo-container">
-//         <form onSubmit={this.onAddTask}>
-//           <input
-//             placeholder="Type your task"
-//             onChange={this.onChange}
-//             value={this.state.value}
-//           />
-//           <button type="submit">Add task</button>
-//         </form>
-//         <ul>{tempList}</ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
---
+++ my-react-app/src/taskReducers.js
@@ -0,0 +1,16 @@
+const initialState = {
+  tasks: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+export default taskReducer;
